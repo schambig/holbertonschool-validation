ba21497146e0e08b6760c855491472ec336b0b38
Salomon Chambi <salomonchambi@gmail.com>
2023-01-07 23:32:39 -0500

    fix: install `zip` and `markdownlint` tools

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -7,5 +7,9 @@ tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
 mv hugo /usr/local/bin/
 rm hugo_extended_0.109.0_Linux-64bit.tar.gz
 
+# Install markdownlint and zip tools
+apt-get install zip -y
+npm install -g markdownlint-cli -y
+
 # Generate a Go-Hugo website
 make build
