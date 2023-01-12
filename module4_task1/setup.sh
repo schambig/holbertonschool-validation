b3fa4ff242d97970ebeabcb6ff27f8d16d0239f6
Salomon Chambi <salomonchambi@gmail.com>
2023-01-12 13:49:33 -0500

    feat: execute the right commands

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -5,7 +5,7 @@ apt-get update && apt-get install -y make wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
 tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
 mv hugo /usr/local/bin/
-rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+rm -f hugo_extended_0.109.0_Linux-64bit.tar.gz || true
 
 # Install markdownlint and zip tools
 apt-get install zip -y
@@ -13,3 +13,13 @@ npm install -g markdownlint-cli -y
 
 # Generate a Go-Hugo website
 make build
+
+# Uninstall go
+sudo apt-get remove golang-go
+rm -rf /usr/local/go || true
+
+# Clean environment files and directory
+rm -rf dist/ || true
+rm -f awesome-api || true
+rm -f coverage-units.out || true
+rm -f coverage-integrations.out || true
