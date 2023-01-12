59d9a3af6d95e0ce7952457b1965cef93a3e28ec
Salomón Chambi G <98289735+schambig@users.noreply.github.com>
2023-01-12 15:35:49 -0500

    fix: install make command

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Install appropriate version of hugo to be run in docker container 'ubuntu 18.04'
-apt-get update && apt-get install -y make wget
+apt-get update && apt-get install -y make
+# apt-get install -y wget
 # wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
 # tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
 # mv hugo /usr/local/bin/
